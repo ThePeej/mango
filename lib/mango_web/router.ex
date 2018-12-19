521f3ce4a01f80d274e4c5db81ec4c063ca132e8
PJ Frias <pj@SmartLogic.io>
2018-12-19 14:41:06 -0500

    add checkout page and refactor router for authentication scopes

--- lib/mango_web/router.ex
+++ lib/mango_web/router.ex
@@ -7,27 +7,38 @@ defmodule MangoWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug MangoWeb.Plugs.LoadCustomer
-    plug MangoWeb.Plugs.FetchCart
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", MangoWeb do
-    pipe_through :browser
+  pipeline :frontend do
+    # Add plugs related to frontend
+    plug MangoWeb.Plugs.LoadCustomer
+    plug MangoWeb.Plugs.FetchCart
+  end
 
-    get "/", PageController, :index
-    get "/categories/:name", CategoryController, :show
+  scope "/", MangoWeb do
+    pipe_through [:browser, :frontend]
+
     get "/register", RegistrationController, :new
     post "/register", RegistrationController, :create
     get "/login", SessionController, :new
     post "/login", SessionController, :create
-    get "/logout", SessionController, :delete
+
+    get "/", PageController, :index
+    get "/categories/:name", CategoryController, :show
     post "/cart", CartController, :add
     get "/cart", CartController, :show
     put "/cart", CartController, :update
+  end
+
+  scope "/", MangoWeb do
+    pipe_through [:browser, :frontend, MangoWeb.Plugs.AuthenticateCustomer]
+
+    get "/logout", SessionController, :delete
+    get "/checkout", CheckoutController, :edit
   end
 
   # Other scopes may use custom stacks.
